1f030eee58f482e600103724e244db48595ef46e
nicolasmironramos <144922077+nicolasmironramos@users.noreply.github.com>
2023-10-11 14:49:54 +0000

    qwertyui

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse
 
 def index(request):
     context = {
         "title": "Django example",
     }
     return render(request, "index.html", context)
+
+def home(request):
+    return HttpResponse("Inicio")
+
+def about(request):
+    return HttpResponse("Historia")
+
+def services(request):
+    return HttpResponse("Servicios")
+
+def store(request):
+    return HttpResponse("Visítanos")
+
+def contact(request):
+    return HttpResponse("Contacto")
+
+def blog(request):
+    return HttpResponse("Blog")
+
+def sample(request):
+    return HttpResponse("Sample")
